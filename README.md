ea3877b45c5efd44b79a6e72b4fd04a82514569e
tenthirtyone <Spactigus@gmail.com>
2017-06-14 23:55:44 -0400

    First build

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# wsp-erc20-blind-guardian
-Safe storage for ether
+# Blind Guardian
+The Blind Guardian is a smart contract that guards ether. It allows for the transfer of Ether and for a locktime to be set. The ethereum only may retrieve their funds after the given locktime passes. Each depositor may create a multi key fallback. E.g. 2 of 3 other accounts may signal to the contract to the delay the fund dispersement and/or defer withdrawal to a fallback address.
+
+It can be used to protect assets in the event that a high value hodler were to travel through a hostile area they may designate two unknown fallbacks who trigger the delay of fund withdrawal in the event of kidnapping. This absolves the Ether holder of the risk of extortion or the famous XKCD monkey wrench interrogation method of defeating strong crypto.
+
+Less grimm applications include fund dispersement over time for trusts, allowance, basic income payments. It should be possible to work a deal with a bank where the hodler stashes N Ether with the blind guardian in exchange for a credit line / debit card that allows them to make visa-like purchases at the spot price of Ether. After the locktime resolves, settle with the ammount due and start again.
+
+## Usage
+
+To initialize a project with this exapmple, run `truffle init webpack` inside an empty directory.
+
+## Building and the frontend
+
+1. First run `truffle compile`, then run `truffle migrate` to deploy the contracts onto your network of choice (default "development").
+1. Then run `npm run dev` to build the app and serve it on http://localhost:8080
---
+++ contracts/BlindGuardian.sol
@@ -0,0 +1,82 @@
+pragma solidity ^0.4.11;
+
+import './StandardToken.sol';
+
+contract BlindGuardian {
+	address owner;														// Depositor
+	address withdrawalAddress;								// Where funds go if nothing goes wrong
+	address panicAddress;											// Where funds go if failsafes trigger override
+	uint256 balance;													// Amount deposited
+	bool guarding = false;										// True while contract guards Ether
+	address[] failsafeList;										// List of trusted failsafes
+	mapping (address => bool) isFailsafe;			// Easier than creating a function
+	mapping (address => bool) panicSwitches;  // Track failsafe panic
+	uint256 panicLimit;												// Number of failsafes to trigger panic mode
+	uint256 startTime;												// When guarding begins
+	uint256 lockTime;													// Time to store Ether
+	uint256 panicTime;												// Additional storage time if in panic
+
+	modifier onlyOwner() {
+		if (msg.sender == owner) _;
+	}
+
+	modifier onlyFailsafe() {
+		if (isFailsafe[msg.sender] == true) _;
+	}
+
+	function () payable {
+		balance += msg.value;
+	}
+
+	function BlindGuardian() onlyOwner {
+		owner = msg.sender;
+	}
+
+	function startGuarding(address _withdrawalAddress, address _panicAddress, uint256 _lockTime, uint256 _panicTime) onlyOwner {
+		withdrawalAddress = _withdrawalAddress;
+		panicAddress = _panicAddress;
+		lockTime = _lockTime;
+		panicTime = _panicTime;
+		startTime = now;
+		guarding = true;
+	}
+
+	function addFallback(address _addr) onlyOwner {
+		panicSwitches[_addr] = true;
+		failsafeList.push(_addr);
+	}
+
+	function setPanicLimit(uint256 _limit) onlyOwner {
+		panicLimit = _limit;
+	}
+
+	function panic() onlyFailsafe {
+		overrides[msg.sender] = true;
+	}
+
+	function withdrawal() onlyOwner returns bool {
+		uint256 delay = startTime + lockTime;
+		address addr = withdrawalAddress;
+
+		if (!isPanicMode()) {
+			addr = panicAddress;
+			delay += panicTime;
+		}
+
+		addr.send(balance);
+	}
+
+	function isPanicMode() returns bool {
+		uint256 count = 0;
+		for (uint256 i = 0; i < failsafeList.length; i++) {
+			if (panicSwitches[failesafeList[i]] == true) {
+				count++;
+			}
+		}
+		if (count >= panicLimit) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
